cf7d9f46cfb8657759fe6f3efa095a1b00595791
MICHAEL WATTS <m_watts@cda074ev5815x.local>
2019-01-17 17:27:39 -0500

    Ass2
    Food constructor update

--- assignments/assignment2/js/Food.js
+++ assignments/assignment2/js/Food.js
@@ -10,10 +10,18 @@ class Food extends Agent {
   // Pass arguments on to the super() constructor (e.g. for Agent)
   // Also set a minimum and maximum size for this food object which it
   // will vary between when it resets
-  constructor(x,y,minSize,maxSize) {
+  constructor(x,y,minSize,maxSize,maxSpeed) {
     super(x,y,random(minSize,maxSize),'#55cccc');
     this.minSize = minSize;
     this.maxSize = maxSize;
+    this.maxSpeed = maxSpeed;
+    this.vx = random(maxSpeed);
+    this.vy = random(maxSpeed);
+  }
+
+  updatePosition(){
+    this.vx = random();
+    food.vy = random() ;
   }
 
   // reset()
