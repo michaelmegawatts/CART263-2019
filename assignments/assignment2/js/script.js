75c303461aadf6fec551dfe453aab9bd98a211c0
michaelmegawatts <42945951+michaelmegawatts@users.noreply.github.com>
2019-01-20 15:27:09 -0500

    Ass
    adjsutment in script

--- assignments/assignment2/js/script.js
+++ assignments/assignment2/js/script.js
@@ -40,7 +40,7 @@ function preload() {
 function setup() {
   createCanvas(windowWidth,windowHeight);
   avatar = new Avatar(mouseX,mouseY,AVATAR_MAX_SIZE,AVATAR_SIZE_LOSS_PER_FRAME)
-  food = new Food(random(0,width),random(0,height),FOOD_MIN_SIZE,FOOD_MAX_SIZE);
+  food = new Food(random(0,windowWidth),random(0,windowHeight),FOOD_MIN_SIZE,FOOD_MAX_SIZE);
   noCursor();
 }
 
