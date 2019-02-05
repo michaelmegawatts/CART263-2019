6230277d6cd8049710aa1060e4b089c834c9366a
Michael Watts <m_watts@cslab-pluto.concordia.ca>
2019-02-05 14:10:54 -0500

    P3 setting up Bauhaus page

--- assignments/project1/js/script.js
+++ assignments/project1/js/script.js
@@ -2,10 +2,17 @@
 
 /*****************
 
-Title of Project
-Author Name
+CART 263-2019
+Project 1
+Michael Watts
+
+Title: SisyBauhausPhus
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+
+$( function() {
+    $( "#instructions" ).draggable();
+  } );
