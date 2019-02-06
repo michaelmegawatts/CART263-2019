268a2a9703f88b148bacd1890ace3004dfcee45b
Michael Watts <m_watts@cslab-pluto.concordia.ca>
2019-02-06 14:22:38 -0500

    P3 Making masters for each object
    Creating masters for each game object so a copy is made each time one is dragged.

--- assignments/project1/js/script.js
+++ assignments/project1/js/script.js
@@ -12,24 +12,50 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 // ******************/
+// Load the Berlin music into a variable
+let music = new Audio('sounds/Berlin.mp3');
 
-// Create function for each object to be draggable
-$( function() {
-  $( "#bauhaus" ).draggable();
-} );
+$(document).ready(function() {
 
-$( function() {
-  $( "#circle" ).draggable();
-} );
+  // Pause the music at the beginning
+  music.pause();
 
-$( function() {
-  $( "#triangle" ).draggable();
-} );
+});
 
-$( function() {
-  $( "#square" ).draggable();
-} );
+// Handle when user mouses over game objects to drag it and make it draggable
+// Create "masters" of each game object to make a coper after each time one is dragged
+$('#content').on('mouseover', '.master', function () {
+  $(this).draggable({
+    // The start property takes a function that is called when dragging starts
+    start: function () {
+      
+      // Create function for each object to be draggable
+      if ($(this).attr('class').indexOf('bauhaus') != -1) {
+        $('#content').append('<div class="master bauhaus"></div>');
 
-$( function() {
-  $( "#ex" ).draggable();
-} );
+        //$( function() {
+        //$( "#bauhaus" ).draggable();
+        //} );
+
+        $( function() {
+          $( "#circle" ).draggable();
+        } );
+
+        $( function() {
+          $( "#triangle" ).draggable();
+        } );
+
+        $( function() {
+          $( "#square" ).draggable();
+        } );
+
+        $( function() {
+          $( "#ex" ).draggable();
+        } );
+
+        // Now we can safely make the one we're dragging not the master
+        $(this).removeClass('master');
+      }
+    }
+  });
+});
