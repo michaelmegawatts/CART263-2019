545ddf7b476c42759e40942ef9603f199b8a1336
michaelmegawatts <42945951+michaelmegawatts@users.noreply.github.com>
2019-02-06 09:30:52 -0500

    P3 Set up page and draggable images

--- assignments/project1/js/script.js
+++ assignments/project1/js/script.js
@@ -6,14 +6,30 @@ CART 263-2019
 Project 1
 Michael Watts
 
-Title: SisyBauhausPhus
+Title: BAUHAUS your life
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
 
 // ******************/
 
+// Create function for each object to be draggable
+$( function() {
+  $( "#bauhaus" ).draggable();
+} );
 
 $( function() {
-    $( "#circle" ).draggable();
-}
+  $( "#circle" ).draggable();
+} );
+
+$( function() {
+  $( "#triangle" ).draggable();
+} );
+
+$( function() {
+  $( "#square" ).draggable();
+} );
+
+$( function() {
+  $( "#ex" ).draggable();
+} );
