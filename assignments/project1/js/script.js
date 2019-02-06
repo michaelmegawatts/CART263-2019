79ec77698c823f82cefb61deb458b0b50fb5fafc
Michael Watts <m_watts@cslab-pluto.concordia.ca>
2019-02-06 14:44:20 -0500

    P3 Indent script

--- assignments/project1/js/script.js
+++ assignments/project1/js/script.js
@@ -22,33 +22,33 @@ $(document).ready(function() {
 
 
 
-// Handle when user mouses over game objects to drag it and make it draggable
-// Create "masters" of each game object to make a coper after each time one is dragged
-$('#content').on('mouseover', '.master', function () {
-  $(this).draggable({
-    // The start property takes a function that is called when dragging starts
-    start: function () {
-      console.log (this);
-      // Create function for each object to be draggable
-      if ($(this).attr('class').indexOf('bauhaus') != -1) {
-        $('#content').append('<div class="master bauhaus"><img src="assets/images/bauhaus.png" alt=""></div>');
+  // Handle when user mouses over game objects to drag it and make it draggable
+  // Create "masters" of each game object to make a coper after each time one is dragged
+  $('#content').on('mouseover', '.master', function () {
+    $(this).draggable({
+      // The start property takes a function that is called when dragging starts
+      start: function () {
+        console.log (this);
+        // Create function for each object to be draggable
+        if ($(this).attr('class').indexOf('bauhaus') != -1) {
+          $('#content').append('<div class="master bauhaus"><img src="assets/images/bauhaus.png" alt=""></div>');
         }
-      if ($(this).attr('class').indexOf('circle') != -1) {
+        if ($(this).attr('class').indexOf('circle') != -1) {
           $('#content').append('<div class="master circle"><img src="assets/images/circle.png" alt=""></div>');
-          }
-      if ($(this).attr('class').indexOf('triangle') != -1) {
+        }
+        if ($(this).attr('class').indexOf('triangle') != -1) {
           $('#content').append('<div class="master triangle"><img src="assets/images/triangle.png" alt=""></div>');
-          }
-      if ($(this).attr('class').indexOf('square') != -1) {
+        }
+        if ($(this).attr('class').indexOf('square') != -1) {
           $('#content').append('<div class="master square"><img src="assets/images/square.png" alt=""></div>');
-          }
-      if ($(this).attr('class').indexOf('ex') != -1) {
+        }
+        if ($(this).attr('class').indexOf('ex') != -1) {
           $('#content').append('<div class="master ex"><img src="assets/images/ex.png" alt=""></div>');
-          }
+        }
         // Now we can safely make the one we're dragging not the master
         $(this).removeClass('master');
 
-    }
+      }
+    });
   });
-});
 });
