2b5d10b21d1d3a193672d4f35e9a09aaa52b895b
Michael Watts <m_watts@cslab-pluto.concordia.ca>
2019-02-14 14:34:23 -0500

    Ass4 poo movement
    Revert property of poo with sound effect.

--- assignments/assignment4/js/script.js
+++ assignments/assignment4/js/script.js
@@ -17,6 +17,7 @@ Chewing: https://freesound.org/people/InspectorJ/sounds/412068/
 // Sound effects for the experience
 let buzzSFX = new Audio("assets/sounds/buzz.mp3");
 let crunchSFX = new Audio("assets/sounds/crunch.wav");
+let grossSFX = new Audio("assets/sounds/gross.mp3");
 
 // Variable to hold our three key elements
 let $mouth;
@@ -57,34 +58,37 @@ function setup() {
 // The arguments 'event' and 'ui' are automatically passed by jQuery UI and contain
 // helpful information about the event.
 function flyDropped (event,ui) {
-  // When we drop the fly into the mouth we should remove the fly from the page
-  // ui contains a reference to the draggable element that was just dropped in ui.draggable
-  // .remove() removes the select element from the page
-  ui.draggable.remove(); // $fly.remove() would work here too
-  // We should "close the mouth" by changing its image
-  // .attr() lets us change specific attributes on HTML element by specifying the attribute
-  // and then what we want to set it to - in this case the 'src' attribute to the closed image
-  $(this).attr('src','assets/images/mouth-closed.png');
-  // Now the fly is gone we should stop its buzzing
-  buzzSFX.pause();
-  // And start the crunching sound effect of chewing
-  crunchSFX.play();
-  // Use a setInterval to call the chew() function over and over
-  setInterval(chew,250);
+  let elementDropped = ui.draggable[0].id;
+  console.log(elementDropped);
+  if (elementDropped=== "fly") {
+    // When we drop the fly into the mouth we should remove the fly from the page
+    // ui contains a reference to the draggable element that was just dropped in ui.draggable
+    // .remove() removes the select element from the page
+    ui.draggable.remove(); // $fly.remove() would work here too
+    // We should "close the mouth" by changing its image
+    // .attr() lets us change specific attributes on HTML element by specifying the attribute
+    // and then what we want to set it to - in this case the 'src' attribute to the closed image
+    $(this).attr('src','assets/images/mouth-closed.png');
+    // Now the fly is gone we should stop its buzzing
+    buzzSFX.pause();
+    // And start the crunching sound effect of chewing
+    crunchSFX.play();
+    // Use a setInterval to call the chew() function over and over
+    setInterval(chew,250);
+  }
 }
 
+// Allows the goldpoo to be dragged, returned to its spot, and play
+// disgust sound effect
 $( function() {
-  $( "#goldpoo" ).draggable({ revert: true });
+  $( "#goldpoo" ).draggable({ revert: "valid",
+  start: function (){
+    grossSFX.play();
+  }
+});
 
-  $( function() {
-  $( "#goldpoo , #goldpoo-nonvalid" ).droppable({
-      accept: "#goldpoo",
-      classes: {
-        "ui-droppable-active": "ui-state-active",
-        "ui-droppable-hover": "ui-state-hover"
-      },
-    });
-    });
+
+$( "#fly" ).draggable({ revert: "invalid" });
 } );
 
 // chew()
