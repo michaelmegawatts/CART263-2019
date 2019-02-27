1b5f67192ce4f5f2bc545dad3706f5efee42718b
Michael Watts <m_watts@cslab-pluto.concordia.ca>
2019-02-27 14:15:12 -0500

    P2 - button disappears

--- assignments/project2/js/script.js
+++ assignments/project2/js/script.js
@@ -14,9 +14,29 @@ const soundscape = new Audio('assets/sounds/soundscape.mp3');
 
 $(document).ready(function() {
 
-  // Effect for instruction dancer to magically fade away after bouncing
-  $( "#intro").click(function() {
-   $( ".content" ).fadeOut( "slow", function() {
-    });
+  //init the vars
+  let video = document.getElementById("myVideo");
+  let btn = document.getElementById("intro");
+  let btnImg = document.getElementById("clickMe");
+  btnImg.addEventListener("click",myFunction);
 
-}
+  function myFunction(){
+
+    if(video.paused){
+      video.play();
+      video.loop = true;
+
+    } else {
+      video.pause();
+
+    }
+      btn.style.display = "none";
+  }
+
+//
+//   // Effect for instruction dancer to magically fade away after bouncing
+//   $( "#intro").click(function() {
+//    $( ".content" ).fadeOut( "slow", function() {
+//     });
+//
+})
