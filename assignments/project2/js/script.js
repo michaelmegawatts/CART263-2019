47a936f82a67fc5bc9dfd14db1fd7fad2413b28c
Michael Watts <m_watts@cslab-pluto.concordia.ca>
2019-02-28 15:52:08 -0500

    P3 - set up array for god questions
    array for questions
    annyang set up for responses and images

--- assignments/project2/js/script.js
+++ assignments/project2/js/script.js
@@ -9,35 +9,38 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
-
+// Ambient sound for experience, will be engaged in the videoFunction when user
+// clicks to begin
 const soundscape = new Audio('assets/sounds/soundscape.mp3');
-
+// Array for responses to god (annyang)
 let answerToGod = [
-"dog",
-"cats",
+  "dog",
+  "cats",
 ]
 
-let showFlickr = function(tag) {
-  //let url = 'https://api.flickr.com/services/rest/?tags='+tag;
-   var url = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-  //$.getJSON(url);
-  console.log(tag);
-  $.getJSON( url, {
-   tags:tag,
-   tagmode: "any",
-   format: "json"
- })
-   .done(function( data ) {
-     console.log(data.items[0].media.m);
-     let responseImg = document.getElementById("responseImage");
-     responseImg.src = data.items[0].media.m;
-});
-}
+// Array for series of questions
+let questions = [
+  "What is your name? say, My name is...",
+  "Tell me your age? say, I am...",
+  "What country are you from? say I am from...",
+  "Do you like dogs or cats? say, I like...",
+  "Do you like red or blue? say, I like...",
+  "What is your favourite flower? say, My favourite is...",
+  "Do you prefer spaghetti or pizza? say, I prefer...",
+  "Chocolate or vanilla icecream? say, I like ...",
+  "Do you like bananas or pears? say, I like...",
+  "What is your astrological sign? say, I am ...",
+  "Do you prefer Lady Gaga or Britney Spears? say, I prefer ...",
+
+]
 
 
+// Set up for introduction to experience. The button click will engage video, soundscape
+// and first question in the series that will come our like a typewriter
 $(document).ready(function() {
   let i = 0;
-  let txt = 'Do you like dogs or cats?';
+  let currentQuestion = 0;
+  let txt = questions[0];
   let speed = 50;
   //init the vars
   let video = document.getElementById("myVideo");
@@ -45,28 +48,67 @@ $(document).ready(function() {
   let btnImg = document.getElementById("clickMe");
   let typebutton = document.getElementById("typebutton");
   typebutton.addEventListener("click",typeWriter);
-  btnImg.addEventListener("click",videoFunction);
+  typebutton.style.display = "none";
+  btnImg.addEventListener("click",gameStart);
 
-  function videoFunction(){
-
+  // function to play video and sound with a click for the introduction
+  function gameStart(){
     if(video.paused){
       video.play();
+      soundscape.play();
       video.loop = true;
+      soundscape.loop = true;
     } else {
       video.pause();
     }
-      btn.style.display = "none";
+    btn.style.display = "none";
+    typebutton.style.display = "block";
   }
 
+  // god will speak using type writer animationd
   function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+    typebutton.style.display = "none";
+    if (i < txt.length) {
+      document.getElementById("demo").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
   }
-}
 
-// Make sure annyang is available...
+
+  // set up response to god using images through flickr that will appear on screen
+  let answerImageFunction = function(tag) {
+    //let url = 'https://api.flickr.com/services/rest/?tags='+tag;
+    var url = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    //$.getJSON(url);
+    console.log(tag);
+    $.getJSON( url, {
+      tags:tag,
+      tagmode: "any",
+      format: "json"
+    })
+    .done(function( data ) {
+      console.log(data.items[0].media.m);
+      let responseImg = document.getElementById("responseImage");
+      responseImg.src = data.items[0].media.m;
+
+      //resets for the next question
+      resetNextQuestion();
+    });
+  }
+
+  // Calculates currrent question and then resets for the next question
+  function resetNextQuestion() {
+    i=0;
+    currentQuestion += 1;
+    txt = questions[currentQuestion];
+    document.getElementById("demo").innerHTML ="";
+    typeWriter();
+  }
+
+
+
+  // set up for annyang
   if (annyang) {
 
     // Add the commands to annyang. That is it should listen
@@ -75,7 +117,14 @@ $(document).ready(function() {
     // variable names means that annyang will call the function
     // specified with EVERYTHING it heard from that point on...
     var command = {
-    'I like *tag': showFlickr
+      'My name is *tag': answerImageFunction,
+      'I am *tag': answerImageFunction,
+      'I am from *tag': answerImageFunction,
+      'I like *tag': answerImageFunction,
+      'I enjoy *tag': answerImageFunction,
+      'My favorite flower is *tag': answerImageFunction,
+      'I prefer *tag': answerImageFunction,
+
     };
 
     // Now we've defined the commands we give them to annyang
@@ -86,4 +135,7 @@ $(document).ready(function() {
     // .start() function
     annyang.start();
   }
+
+
+
 });
