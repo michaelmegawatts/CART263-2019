abeb1b514f709e6e7b99cf20ec2107656e388877
michaelmegawatts <42945951+michaelmegawatts@users.noreply.github.com>
2019-03-12 08:37:04 -0400

    Update script.js

--- assignments/project2/js/script.js
+++ assignments/project2/js/script.js
@@ -203,7 +203,7 @@ $(document).ready(function() {
 
   function gameOver(){
     console.log("whatever");
-    if (currentQuestion === 1) {
+    if (currentQuestion === 30) {
       endButton.style.display = "block";
     }
     else {
