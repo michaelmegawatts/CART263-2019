c7444862a2f74e91e950e742c80c29891240c973
MICHAEL WATTS <m_watts@cda074ev5815x.local>
2019-04-11 16:14:02 -0400

    P3 responsive voice
    Set up responsive voice for responses to when user answers

--- assignments/project3/js/script.js
+++ assignments/project3/js/script.js
@@ -2,21 +2,19 @@
 
 /*****************
 
-Title of Project: Puzzle Earth
+Title of Project: Earth Collage
 Michael
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
 
-button photo: https://unsplash.com/photos/BPiQ25xU6ak
-earth photo: https://www.nasa.gov/topics/earth/index.html
-polar bear: https://defenders.org/climate-change/overview
-mountain: Kalen Emsley https://unsplash.com/photos/Bkci_8qcdvQ?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText
+
 ******************/
 
 
 const data = []
 
+let zIndex = 0;
 
 // Class for series of questions
 class EarthQuestion {
@@ -34,49 +32,47 @@ class EarthQuestion {
   }
 
   //Draw images using stamp function, rotate and scale
-   display (){
-    push();
-    translate(this.x,this.y);
-    rotate(this.stampAngle);
-    scale(this.stampSize);
-    image(this.stampImage,0,0);
-    pop();
-  }
+  //  display (){
+  //   push();
+  //   translate(this.x,this.y);
+  //   rotate(this.stampAngle);
+  //   scale(this.stampSize);
+  //   image(this.stampImage,0,0);
+  //   pop();
+  // }
 }
 
-data.push(new EarthQuestion("Are you from planet Earth?","Best planet of the universe!","true","false","earth.png"));
+data.push(new EarthQuestion("Are you from planet Earth?","Best planet of the universe!","true","false","mountain.png"));
 data.push(new EarthQuestion("Is the earth flat?","phewf!","true","false","polarbear.png"));
-data.push(new EarthQuestion("Do scientists agree on climate change?","A vast majority do","true","false","mountain.png"));
+data.push(new EarthQuestion("Do scientists agree on climate change?","A vast majority do","true","false","earth.png"));
 
 // Set up for introduction to experience. The button click will engage video, soundscape
 // and first question in the series
 $(document).ready(function() {
   let i = 0;
-  let currentQuestion = 0;
   let txt = data[0].question;
-
-  let currentStamp ;
   let speed = 100;
+  let currentQuestion = 0;
+  let currentStamp ;
+  let questionNumber = 0;
+  let voice = data[0].answer;
+  let earthAnswer = 0;
 
   let video = document.getElementById("myVideo");
   let btn = document.getElementById("intro");
   let btnImg = document.getElementById("clickMe");
   let typebutton = document.getElementById("typebutton");
   typebutton.addEventListener("click",typeWriter);
-
   typebutton.style.display = "none";
   btnImg.addEventListener("click",gameStart);
+
   let checkBoxY = document.getElementById("myCheckY");
-    checkBoxY.addEventListener("click",positiveA);
+  checkBoxY.addEventListener("click",positiveA);
   let checkBoxN = document.getElementById("myCheckN");
-    checkBoxN.addEventListener("click",negativeA);
+  checkBoxN.addEventListener("click",negativeA);
 
-let stampContainer = document.getElementById("stampImage");
+  let stampContainer = document.getElementById("stampImage");
 
-//stampContainer.src = currentStamp;
-let questionNumber = 0;
-
-  //btnAnswer.addEventListener("click",playerAnswer);
   // function to play video and sound with a click for the introduction
   function gameStart(){
     if(video.paused){
@@ -91,8 +87,7 @@ let questionNumber = 0;
     typebutton.style.display = "block";
   }
 
-  // Earth will communicate using type writer animation.
-
+  // Earth will ask questions using type writer animation.
   function typeWriter() {
     typebutton.style.display = "none";
     if (i < txt.length) {
@@ -102,97 +97,82 @@ let questionNumber = 0;
     }
   }
 
+  // Set up response for user... considered "positive" or Agree as a checkbox
   function positiveA() {
     console.log("checked");
     var checkBox = document.getElementById("myCheckY");
     var text = document.getElementById("text");
     if (checkBox.checked == true){
-       text.style.display = "block";
-     } else {
-        text.style.display = "none";
-     }
-     currentStamp = "assets/images/"+data[questionNumber].stamp;
-     stampContainer.src = currentStamp;
-         $("#draggableImageContainer").css({ display:'block'});
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+    console.log(data[earthAnswer].answer);
+    speakAnswer(data[earthAnswer].answer);
+    currentStamp = "assets/images/"+data[questionNumber].stamp;
+    stampContainer.src = currentStamp;
+    $("#draggableImageContainer").css({ display:'block', zIndex: 1});
   }
 
+// Set up response for user... considered "negative" or Disagree as a checkbox
   function negativeA() {
     var checkBox = document.getElementById("myCheckN");
     var text = document.getElementById("text");
     if (checkBox.checked == true){
       text.style.display = "block";
     } else {
-       text.style.display = "none";
+      text.style.display = "none";
     }
+    console.log(data[earthAnswer].answer);
+    speakAnswer(data[earthAnswer].answer);
     currentStamp = "assets/images/"+data[questionNumber].stamp;
     stampContainer.src = currentStamp;
-        $("#draggableImageContainer").css({ display:'block'});
+    $("#draggableImageContainer").css({ display:'block', zIndex: 1});
   }
 
   // Handle when user mouses over game shapes to drag it and make it draggable
-// code - parts of Beach Party by Pippin Barr but altered code
-
-$('#content').on('mouseover', '.masterImage', function () {
+  // code - parts of Beach Party by Pippin Barr but altered code
+  $('#content').on('mouseover', '.masterImage', function() {
     $(this).draggable({
       // The start property takes a function that is called when dragging starts
       start: function () {
         console.log (this);
-        // Create function for each object to be draggable
-        // if ($(this).hasClass("stamp")) {
-        //   currentStamp = "assets/images/"+data[questionNumber].stamp;
-        //   $('#content').append(' <div class="masterImage stamp"><img id = "stampImage" src="' + currentStamp+'" alt=""></div>');
-        // }
-
-        // Remove master class from the dragged shape
-        // $(this).removeClass('masterImage');
-        // console.log($(this));
       },
     });
   });
 
   $('#content').on('mouseup', '.masterImage', function () {
     console.log ("mouseup");
-  //  $(this).draggable('disable');
-   let previousPos = $(this).position();
-   console.log(previousPos);
+    //  $(this).draggable('disable');
+    let previousPos = $(this).position();
+    console.log(previousPos);
 
     //resets for the next question
     resetNextQuestion(previousPos);
     $('#draggableImageContainer').css({bottom: 0, left: 0, display:'none'});
-    });
+  });
 
 
-    // Calculates currrent question and then resets for the next question
-    function resetNextQuestion(previousPos) {
-      console.log( "in reset"+previousPos.top)
-      i=0;
-      let img = $('<img />').attr({
-            'src': currentStamp,
-        }).appendTo('#content').css({top: previousPos.top, left: previousPos.left, position:'absolute'});
+  // Calculates currrent question and then resets for the next question
+  function resetNextQuestion(previousPos) {
+    console.log( "in reset"+previousPos.top)
+    i=0;
+    let img = $('<img />').attr({
+      'src': currentStamp,
+    }).appendTo('#content').css({top: previousPos.top, left: previousPos.left, position:'absolute'});
 
-      questionNumber++;
-      //gameOver();
-      txt = data[questionNumber].question;
-      document.getElementById("demo").innerHTML ="";
-      typeWriter();
-    }
+    questionNumber++;
+    //gameOver();
+    txt = data[questionNumber].question;
+    document.getElementById("demo").innerHTML ="";
+    typeWriter();
+  }
 
+  function speakAnswer(earthVoice) {
+    responsiveVoice.speak(earthVoice,'UK English Male');
+    earthAnswer++;
 
-
-  // Function for when user answers yes, and then Earth responds
-//function earthAnswerYes(){
-  //console.log("timer expired");
-
-  //if ("answer-1" === ){
-  //earthAnswerYes += 1;
-  //responsiveVoice.speak(gawdSpeak[gawdWaitingSpeak], "UK English Female");
-//  }
-//
-
-// set up for Responsive Voice
-
-
-
+  }
 
 
 
