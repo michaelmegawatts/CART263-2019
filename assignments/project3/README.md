d4ef98907e35686d3abf3af6d633a1ac56435df5
MICHAEL WATTS <m_watts@cda103ev5715x.local>
2019-04-15 14:41:43 -0400

    P3 dialogue boxes
    attempted to add dialogue boxes.
    added more questions and images

--- assignments/project3/README.md
+++ assignments/project3/README.md
@@ -1,7 +1,11 @@
 Project description and credits
 
-sources: https://climate.nasa.gov/faq/
+sources: NASA - https://climate.nasa.gov/faq/
 https://www.earthday.org/climate-change-quiz/
+National Geographic - https://news.nationalgeographic.com/2018/02/polar-bears-starve-melting-sea-ice-global-warming-study-beaufort-sea-environment/
+King Country - https://kingcounty.gov/depts/dnrp/solid-waste/programs/climate/climate-change-recycling.aspx
+Guardian: https://www.theguardian.com/environment/2019/mar/26/climate-change-denial-is-evil-says-mary-robinson
+Clean air: https://www.unenvironment.org/news-and-stories/story/right-breathe-clean-air
 
 Earth Video: edited by Michael Watts
 source: https://pixabay.com/videos/earth-globe-sea-trenches-9183/
@@ -15,3 +19,10 @@ sun: https://twitter.com/sun
 iceberg: http://cwf-fcf.org/en/news-features/articles/climate-change-vs-global.html
 ozone: https://www.express.co.uk/news/science/613084/Ozone-recovery-shock-Nasa-says-hole-in-Ozone-Layer-should-be-half-closed-by-2020
 adam and eve: https://useum.org/artwork/The-First-kiss-of-Adam-and-Eve-Salvador-Viniegra-1891
+flat earth: https://knowyourmeme.com/photos/1323410-flat-earth-theory
+recycle symbol: https://dakofa.com/element/test-article-next-week/
+denial: https://www.momscleanairforce.org/climate-denial-cartoon/
+spaceship house: http://roadsidewonders.net/delaware-spaceship-house/
+mask: https://www.pinterest.ca/pin/303359724894765618/
+puppy: https://oshiprint.in/posters/animals/cute-puppy-3
+burger: https://bluewater.co.uk/food-and-drink
--- assignments/project3/js/script.js
+++ assignments/project3/js/script.js
@@ -30,26 +30,22 @@ class EarthQuestion {
   update (stamp) {
     this.stamp = stamp;
   }
-
-  //Draw images using stamp function, rotate and scale
-  //  display (){
-  //   push();
-  //   translate(this.x,this.y);
-  //   rotate(this.stampAngle);
-  //   scale(this.stampSize);
-  //   image(this.stampImage,0,0);
-  //   pop();
-  // }
 }
 
 data.push(new EarthQuestion("Are you from planet Earth?","Best planet of the universe!","true","false","mountain.png"));
-data.push(new EarthQuestion("Is the earth flat?","phewf, you may continue to exist!","true","false","polarbear.png"));
+data.push(new EarthQuestion("Do you think Earth is a nice place?","even in the freezing winter, huh?","true","false","spaceshiphouse.png"));
+data.push(new EarthQuestion("Is it important to have clean air?","Clean air is fundamental to healthy human life","true","false","mask.png"));
+data.push(new EarthQuestion("Is the earth flat?","phewf, you may continue to exist!","true","false","flatearth.jpg"));
+data.push(new EarthQuestion("Is climate change a hoax?","The denial of climate change is not just ignorant, but malign and evil because it denies the human rights of the most vulnerable people on the planet","true","false","denial.jpg"));
+data.push(new EarthQuestion("Do you have pets?","So do I, but not cats","true","false","puppy.jpg"));
+data.push(new EarthQuestion("Do you enjoy eating tasty food?","Extra bacon please! gimme gimme gimme","true","false","burger.jpg"));
 data.push(new EarthQuestion("Do scientists agree on climate change?","Yes, the vast majority of actively publishing climate scientists – 97 percent – agree that humans are causing global warming and climate change.","true","false","earth.png"));
 data.push(new EarthQuestion("Is the sun causing global warming?","No. The Sun can influence the Earth’s climate, but it isn’t responsible for the warming trend we’ve seen over the past few decades.","true","false","sun.png"));
 data.push(new EarthQuestion("Is the ocean continuing to warm?","​Yes, the ocean is continuing to warm. Notably, all ocean basins have been experiencing significant warming since 1998, with more heat being transferred deeper into the ocean since 1990.","true","false","iceberg.png"));
 data.push(new EarthQuestion("Is the ozone hole causing climate change?","Yes and no. The ozone hole is not causing global warming, but it is affecting atmospheric circulation","true","false","ozone.png"));
 data.push(new EarthQuestion("Is it too late to prevent climate change?","Humans have caused major climate changes to happen already, and we have set in motion more changes still. Even if we stopped emitting greenhouse gases today, global warming would continue to happen for at least several more decades, if not centuries.","true","false","adameve.png"));
-
+data.push(new EarthQuestion("Are polar bears and other animals at risk because of global warming?","Because of melting sea ice, it is likely that more polar bears will soon starve, warns a new study that discovered the large carnivores need to eat 60 percent more than anyone had realized","true","false","polarbear.png"));
+data.push(new EarthQuestion("Does recycling help climate change?","Recycling helps reduce greenhouse gas emissions by reducing energy consumption","true","false","recycle.png"));
 
 // Set up for introduction to experience. The button click will engage video, soundscape
 // and first question in the series
@@ -171,9 +167,12 @@ $(document).ready(function() {
 
   }
 
-  // $( function() {
-  //   $( "#dialog" ).dialog();
-  // } );
+// function dialogBox () {
+//   $( function() {
+//     $( "#dialog" ).dialog();
+//    } );
+// }
+
 
 
 });
